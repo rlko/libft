2042ccce099226a3eeeedef49ffb4c2f46f17b29
Ronald LIOU-KEE-ON <rliou-ke@e1r7p9.42.fr>
2015-12-02 13:35:59 +0100

    prot

--- ft_tabdel.c
+++ ft_tabdel.c
@@ -6,7 +6,7 @@
 /*   By: rliou-ke <rliou-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 12:47:23 by rliou-ke          #+#    #+#             */
-/*   Updated: 2015/12/02 12:47:49 by rliou-ke         ###   ########.fr       */
+/*   Updated: 2015/12/02 13:35:27 by rliou-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ void	ft_tabdel(char **tab, int size)
 	int	i;
 
 	i = 0;
-	while (i <= size)
+	if (tab)
 	{
-		ft_strdel(&tab[i]);
-		i++;
+		while (i <= size)
+		{
+			ft_strdel(&tab[i]);
+			i++;
+		}
 	}
 	free(tab);
 }
