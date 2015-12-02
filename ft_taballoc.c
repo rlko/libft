f178b6118a25b8d1df2c02923da0c3390665c470
Ronald LIOU-KEE-ON <rliou-ke@e1r7p9.42.fr>
2015-12-02 12:28:36 +0100

    deps

--- ft_taballoc.c
+++ ft_taballoc.c
@@ -6,9 +6,12 @@
 /*   By: rliou-ke <rliou-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 10:19:06 by rliou-ke          #+#    #+#             */
-/*   Updated: 2015/12/02 12:23:23 by rliou-ke         ###   ########.fr       */
+/*   Updated: 2015/12/02 12:28:20 by rliou-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
 
 char	**ft_taballoc(size_t size)
 {
