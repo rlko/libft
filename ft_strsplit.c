ca6353693deced41d5612fb285da0b3cc37c2777
Ronald LIOU-KEE-ON <rliou-ke@e1r2p2.42.fr>
2015-12-13 13:11:15 +0100

    absolutely digusplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: rliou-ke <rliou-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/10 17:03:09 by rliou-ke          #+#    #+#             */
-/*   Updated: 2015/05/20 07:40:49 by rliou-ke         ###   ########.fr       */
+/*   Updated: 2015/12/13 13:10:51 by rliou-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,15 @@ char			**ft_strsplit(char const *s, char c)
 
 	i = 0;
 	j = 0;
-	if (!s || s == 0)
-		return (0);
-	tab = (char**)malloc(sizeof(tab) * ft_count_word(s, c) + 1);
+	if (!s || !(tab = malloc(sizeof(char **) * ft_count_word(s, c) + 1)))
+		return (NULL);
 	tab[ft_count_word(s, c)] = NULL;
 	while (s[i])
 	{
 		if (s[i] != c)
 		{
-			tab[j] = (char*)malloc(sizeof(tab) * ft_count_letter(s, c, i));
+			if (!(tab[j] = malloc(sizeof(char *) * ft_count_letter(s, c, i))))
+				return (NULL);
 			tab[j] = ft_strsub(s, i, ft_count_letter(s, c, i));
 			j++;
 			while (s[i] != c && s[i])
