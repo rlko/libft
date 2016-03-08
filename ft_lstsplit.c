97d82b857869bc3a5220acfdc6e3e1a77eec75cb
Ronald LIOU-KEE-ON <rliou-ke@e2r6p8.42.fr>
2016-03-08 06:37:41 +0100

    ft_lstsplit.c re

--- ft_lstsplit.c
+++ ft_lstsplit.c
@@ -6,22 +6,21 @@
 /*   By: rliou-ke <rliou-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/23 03:12:01 by rliou-ke          #+#    #+#             */
-/*   Updated: 2016/02/27 18:14:28 by rliou-ke         ###   ########.fr       */
+/*   Updated: 2016/03/08 06:36:48 by rliou-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static int		delimit(char *str, int delimchar, int i)
+static int		delimit(char *str, int delimichar, int i)
 {
-	while (str[i] != '\0')
-	{
-		if (str[i] == delimchar)
-			return (++i);
-		i++;
-	}
-	return (i);
+	int			j;
+
+	j = i;
+	while (str[j] != delimichar && str[j] != '\0')
+		j++;
+	return (j);
 }
 
 static t_list	*mk_path_node(char *str, int i, int j)
@@ -31,7 +30,7 @@ static t_list	*mk_path_node(char *str, int i, int j)
 	node = NULL;
 	if (!(node = malloc(sizeof(*node))))
 		ft_exit_error("mk_path_node: malloc: node");
-	node->content_size = j - i - 1;
+	node->content_size = j - i;
 	if (!(node->content = ft_strsub(str, i, node->content_size)))
 		ft_exit_error("mk_path_node: ft_strsub: node->content");
 	node->next = NULL;
@@ -64,7 +63,6 @@ t_list			*ft_lstsplit(char *str, int c)
 {
 	int			i;
 	int			j;
-	int			len;
 	t_list		*list;
 	t_list		*head;
 
@@ -72,16 +70,19 @@ t_list			*ft_lstsplit(char *str, int c)
 		return (NULL);
 	head = NULL;
 	i = 0;
-	j = 0;
-	len = ft_strlen(str);
-	while (i != len)
+	while (str[i] != '\0')
 	{
-		j = delimit(str, c, i);
-		if ((list = mk_path_node(str, i, j == len ? j + 1 : j)))
-			head = add_node(head, list);
-		while (str[j] && str[j] == c)
-			j++;
-		i = j;
+		if (str[i] != c)
+		{
+			j = delimit(str, c, i);
+			if ((list = mk_path_node(str, i, j)))
+				head = add_node(head, list);
+			while (str[j] && str[j] == c)
+				j++;
+			i = j;
+		}
+		else
+			i++;
 	}
 	return (head);
 }
