d9bdeaae82388e10c7eb7cce2c9c34bd06839122
Lon <lon@xevo.home>
2016-03-14 09:46:33 +0100

    Modified: ft_strlen w/o index

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: rliou-ke <rliou-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 10:48:29 by rliou-ke          #+#    #+#             */
-/*   Updated: 2015/11/23 10:44:53 by rliou-ke         ###   ########.fr       */
+/*   Updated: 2016/03/14 09:46:02 by rliou-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-size_t	ft_strlen(const char *s)
+size_t		ft_strlen(const char *s)
 {
-	size_t i;
+	char	*tmp;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	tmp = (char *)s;
+	while (*tmp)
+		*tmp++;
+	return (tmp - s);
 }
